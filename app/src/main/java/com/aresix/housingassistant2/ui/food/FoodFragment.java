393c34e6e656ca98f3e0bdd4bbea961518856710
Aresix <ares_six@hotmail.com>
2020-02-09 18:05:37 +0800

    框架雏形完成0.1.4 第二个页面全完成 （下期预告 第三页面只会加入简单的百度地图的界面 无其他操作

--- app/src/main/java/com/aresix/housingassistant2/ui/food/FoodFragment.java
+++ app/src/main/java/com/aresix/housingassistant2/ui/food/FoodFragment.java
@@ -18,6 +18,10 @@ import android.view.ViewGroup;
 import android.widget.AdapterViewFlipper;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aresix.housingassistant2.R;
 import com.aresix.housingassistant2.adapter.MyFlipperAdapter;
@@ -25,13 +29,18 @@ import com.aresix.housingassistant2.adapter.MyFlipperAdapter;
 public class FoodFragment extends Fragment {
 
     private FoodViewModel mViewModel;
-    private int[] mFlipPic={
-            R.drawable.test02,R.drawable.test01,R.drawable.test03,
-            R.drawable.test04,R.drawable.test05
+    private int[] mFlipPic = {
+            R.drawable.test02, R.drawable.test01, R.drawable.test03,
+            R.drawable.test04, R.drawable.test05
     };
     private AdapterViewFlipper mFlipper;
     private ImageView[] pots;
     private static final String TAG = "Heliosssss";
+    private RelativeLayout[] mRL3 = {null, null, null, null, null};
+    private RelativeLayout[] mRL2 = {null, null};
+    private RelativeLayout mRLCard = null;
+    private TextView mTvCardTitle = null, mTvCardContent = null;
+    private ImageView mBtnCard=null;
 
     public static FoodFragment newInstance() {
         return new FoodFragment();
@@ -40,9 +49,10 @@ public class FoodFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_food, container, false);
-         mFlipper = view.findViewById(R.id.flipper);
+        View view = inflater.inflate(R.layout.fragment_food, container, false);
+        mFlipper = view.findViewById(R.id.flipper);
 
+        /* ======== initialize ======== */
         pots = new ImageView[]{
                 view.findViewById(R.id.PicCarousel_img0),
                 view.findViewById(R.id.PicCarousel_img1),
@@ -53,11 +63,95 @@ public class FoodFragment extends Fragment {
         Button btnLeft = view.findViewById(R.id.prevPic);
         Button btnRight = view.findViewById(R.id.nextPic);
 
-        MyFlipperAdapter adapter = new MyFlipperAdapter(getContext(),mFlipPic,pots,btnLeft,btnRight);
+        mRL2[0] = view.findViewById(R.id.leftFood);
+        mRL2[1] = view.findViewById(R.id.rightFood);
+
+        mRL3[0] = view.findViewById(R.id.foodItem0301);
+        mRL3[1] = view.findViewById(R.id.foodItem0302);
+        mRL3[2] = view.findViewById(R.id.foodItem0303);
+        mRL3[3] = view.findViewById(R.id.foodItem0304);
+        mRL3[4] = view.findViewById(R.id.foodItem0305);
+
+        mRLCard = view.findViewById(R.id.foodCard);
+        mTvCardContent = view.findViewById(R.id.cardContent);
+        mTvCardTitle = view.findViewById(R.id.cardTitle);
+        mBtnCard=view.findViewById(R.id.close);
+        /* ======== initialize ======== */
+
+        MyFlipperAdapter adapter = new MyFlipperAdapter(getContext(), mFlipPic, pots, btnLeft, btnRight);
         mFlipper.setAdapter(adapter);
 
-        btnChoose(btnLeft,btnRight);
+        btnChoose(btnLeft, btnRight);
+        rl2Click(mRL2);
+        rl3Click(mRL3, mRLCard, mTvCardTitle, mTvCardContent,mBtnCard);
         return view;
+    }
+
+    private void rl3Click(RelativeLayout[] rl3, final RelativeLayout rlCard,
+                          final TextView tvCardTitle, final TextView tvCardContent, ImageView btnCard) {
+        rl3[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rlCard.setVisibility(View.VISIBLE);
+                tvCardTitle.setText(R.string.lzysFamousDishes);
+                tvCardContent.setText(R.string.lzysFamousDishesContent);
+            }
+        });
+        rl3[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rlCard.setVisibility(View.VISIBLE);
+                tvCardTitle.setText(R.string.loveProducer);
+                tvCardContent.setText(R.string.loveProducerContent);
+            }
+        });
+        rl3[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rlCard.setVisibility(View.VISIBLE);
+                tvCardTitle.setText(R.string.psycho);
+                tvCardContent.setText(R.string.psychoContent);
+            }
+        });
+        rl3[3].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rlCard.setVisibility(View.VISIBLE);
+                tvCardTitle.setText(R.string.souvenir);
+                tvCardContent.setText(R.string.souvenirContent);
+            }
+        });
+        rl3[4].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rlCard.setVisibility(View.VISIBLE);
+                tvCardTitle.setText(R.string.license);
+                tvCardContent.setText(R.string.licenseContent);
+            }
+        });
+
+        //没有必要 判断 card 的状态 因为RelativeLayout不在了 那么button也看不见了
+        btnCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rlCard.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    private void rl2Click(RelativeLayout[] rl2) {
+        rl2[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "欢迎点击-饮食全记录", Toast.LENGTH_SHORT).show();
+            }
+        });
+        rl2[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "欢迎点击-我的收藏", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -67,7 +161,7 @@ public class FoodFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
-    private void btnChoose(Button btnLeft, Button btnRight){
+    private void btnChoose(Button btnLeft, Button btnRight) {
         btnLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
